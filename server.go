e5c1ad8dc72b88350f381c64d5cfe4f507e7a4ae
GUO YANKE <hi@yankeguo.com>
2025-07-02 12:55:00 +0800

    feat: enhance server implementation with service options and error handling
    - Added ServiceOptions struct to manage service configurations
    - Implemented server initialization with service options retrieval
    - Enhanced error handling for invalid frames and service token validation
    - Updated server's Run method to accept connections and handle them concurrently

--- server.go
+++ server.go
@@ -1,22 +1,96 @@
 package quickrelay
 
 import (
+	"errors"
+	"fmt"
 	"net"
 )
 
+var (
+	ErrServiceUnknown = errors.New("service unknown")
+)
+
+type ServiceOptions struct {
+	ServiceName  string
+	ServiceToken string
+	ServicePort  int
+}
+
 type ServerOptions struct {
+	GetServiceOptions func(name string) (ServiceOptions, error)
 }
 
 type Server interface {
 	Run(l net.Listener) error
 }
 
-type server struct{}
+type server struct {
+	opts ServerOptions
+}
 
-func NewServer(opts *ServerOptions) Server {
-	return &server{}
+func NewServer(opts ServerOptions) Server {
+	return &server{opts: opts}
+}
+
+func (s *server) serve(conn net.Conn) {
+	defer conn.Close()
+
+	var f Frame
+	var err error
+
+	fr := NewFrameReader(conn)
+	fw := NewFrameWriter(conn)
+
+	if f, err = fr.ReadFrame(); err != nil {
+		fw.WriteFrame(Frame{
+			FrameType: FrameTypeError,
+			FramePayloadError: &FramePayloadError{
+				Error: fmt.Sprintf("invalid frame: %v", err),
+			},
+		})
+		return
+	}
+
+	if f.FrameType != FrameTypeHandshakeRequest {
+		fw.WriteFrame(Frame{
+			FrameType: FrameTypeError,
+			FramePayloadError: &FramePayloadError{
+				Error: "invalid handshake request",
+			},
+		})
+		return
+	}
+
+	var so ServiceOptions
+
+	if so, err = s.opts.GetServiceOptions(f.ServiceName); err != nil {
+		fw.WriteFrame(Frame{
+			FrameType: FrameTypeError,
+			FramePayloadError: &FramePayloadError{
+				Error: fmt.Sprintf("failed to get service options: %v", err),
+			},
+		})
+		return
+	}
+
+	if f.ServiceToken != so.ServiceToken {
+		fw.WriteFrame(Frame{
+			FrameType: FrameTypeError,
+			FramePayloadError: &FramePayloadError{
+				Error: "invalid service token",
+			},
+		})
+		return
+	}
+
 }
 
 func (s *server) Run(l net.Listener) error {
-	return nil
+	for {
+		conn, err := l.Accept()
+		if err != nil {
+			return err
+		}
+		go s.serve(conn)
+	}
 }
